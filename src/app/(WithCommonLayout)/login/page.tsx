90f6b1bc64479a8fa7f8bf9fbbaac462392b160e
sehabahmed <freelancersehab1@gmail.com>
2025-10-25 23:54:32 +0200

    feat (common layout): create register, login, admin user pages

---
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Login() {
+  return (
+    <div>
+      This is Login page
+    </div>
+  )
+}
---
+++ src/app/(WithCommonLayout)/register/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Register() {
+  return (
+    <div>
+      This is Register Page
+    </div>
+  )
+}
