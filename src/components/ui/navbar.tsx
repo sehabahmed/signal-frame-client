f4367b38aca965496f85fe44e9f4f5ab37110f02
sehabahmed <freelancersehab1@gmail.com>
2025-10-28 21:45:24 +0100

    feat (navbar): create navbar file

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,10 @@
+
+export default function navbar() {
+  return (
+        <nav className="fixed inset--x-0 top-0 z-50 h-16 items-center justify-between border-b border-border/10 bg-background-95 backdrop-blur-sm">
+            <div className="flex items-center gap-2">
+    
+            </div>
+        </nav>
+  )
+}
