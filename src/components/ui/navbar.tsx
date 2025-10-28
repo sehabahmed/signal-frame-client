dcf8c6aae97d5fa3432e23dadae66313901c1250
sehabahmed <freelancersehab1@gmail.com>
2025-10-28 21:55:13 +0100

    update (navbar): set demo navbar in UI

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function navbar() {
+export default function Navbar() {
   return (
     <nav className="fixed inset--x-0 top-0 z-50 h-16 items-center justify-between border-b border-border/10 bg-background-95 backdrop-blur-sm">
       <div className="flex items-center gap-2">
         <Link href="/">
           <Image src="/" alt="logo" width={32} height={32} />
+          My Website....
         </Link>
       </div>
     </nav>
