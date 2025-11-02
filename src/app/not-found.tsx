6ee44db3c95ef2e7cc47ce4335c6e2386e6a953a
sehabahmed <freelancersehab1@gmail.com>
2025-11-02 16:41:52 +0100

    update (not found): change not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,28 @@
-import NotFound from "../components/ui/not-found";
 
+import Link from "next/link";
+import React from "react";
 
+// react icons
+import {FaArrowLeftLong} from "react-icons/fa6";
 
-export default function NotFoundDemo() {
-  return <NotFound animate={true} />;
-}
+const NotFound = () => {
+
+    return (
+        <div className="boxShadow px-10 w-full flex items-center flex-col justify-center py-20 rounded-xl bg-[#00543A]">
+            <img src="https://i.ibb.co/LvLq6d3/Group-29.png" alt="illustration"
+                 className="w-full lg:w-[400px]"/>
+            <p className="text-[#fff] text-[1.2rem] w-full lg:w-[55%] text-center">Oops it seems you follow
+                backlink</p>
+
+            <Link href="/">
+                <button
+                className="py-3 px-6 sm:px-8 rounded-full bg-[#fff] text-black mt-4 flex items-center gap-[10px]">
+                <FaArrowLeftLong/> Back to home
+            </button>
+            </Link>
+        </div>
+    );
+};
+
+export default NotFound;
+                    
