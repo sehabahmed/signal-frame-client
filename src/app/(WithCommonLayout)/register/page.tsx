c2472442aa805c02a50f3080ad89d1c349952b59
sehabahmed <freelancersehab1@gmail.com>
2025-11-04 11:07:21 +0100

    update (login form): update login page

--- src/app/(WithCommonLayout)/register/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Register() {
-  return <div>This is Register Page</div>;
-}
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { SingInForm } from "@/src/components/nurui/singin-form";
+
+const SingInFormDemo = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <SingInForm />
+    </div>
+  );
+};
+
+export default SingInFormDemo ;
